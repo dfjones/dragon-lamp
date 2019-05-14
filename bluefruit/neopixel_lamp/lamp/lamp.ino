442d52d691a3d39979d51c33efd7378f6f05e0d2
Doug Jones <djones@squarespace.com>
2019-05-14 00:20:06 -0400

    flicker any base color.

--- bluefruit/neopixel_lamp/lamp/lamp.ino
+++ bluefruit/neopixel_lamp/lamp/lamp.ino
@@ -7,35 +7,39 @@
 // Tell it how many pixels, and which pin to use to send signals.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN_PIXELS, NEO_GRB + NEO_KHZ800);
 
+uint32_t orange = pixels.Color(200, 25, 4);
+uint32_t blue = pixels.Color(4, 25, 200);
+
 // The setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(115200);
   //while (!Serial) {
     // Wait for serial port to connect. Needed for native USB
- //   delay(100);
+    //delay(100);
   //}
 
   // Initialize NeoPixel library
   pixels.begin();
+  pixels.clear();
   
   Serial.println(F("Setup done"));  
 }
 
-int prev = 128;
+int prev = 100;
 int lvl = random(64, 191);
 
 // The loop function runs over and over again forever
 void loop() {
-  flicker(244, 140, 66);
+  flicker(4, 25, 200);
 }
 
-void flicker(int r, int g, int b) {
-  lvl = random(64, 191);
+void flicker(uint8_t r, uint8_t g, uint8_t b) {
+  lvl = random(80, 120);
   split(prev, lvl, 32, r, g, b);
   prev = lvl;
 }
 
-void split(int first, int second, int offset, int r, int g, int b) {
+void split(int first, int second, int offset, uint8_t r, uint8_t g, uint8_t b) {
   //Serial.println("first = " + String(first) + " second = " + String(second) + " offset = " + String(offset));
   if (offset != 0) {
     int mid = ((first + second + 1) / 2 + random(-offset, offset));
@@ -44,11 +48,12 @@ void split(int first, int second, int offset, int r, int g, int b) {
     split(mid, second, offset, r, g, b);
   } else {
     float level = pow(float(first) / 255.0, 2.7) * 255 + 0.5;
+    level = level / 100.0;
     Serial.println("level = " + String(level));
-    uint32_t c = pixels.Color(uint8_t(level), uint8_t(level / 8), uint8_t(level / 48));
+    uint32_t c = pixels.Color(uint8_t(r * level), uint8_t(g * level), uint8_t(b * level));
     pixels.fill(c, 0, 0);
     pixels.show();
-    delay(2);
+    delay(10);
   }
 }
 
